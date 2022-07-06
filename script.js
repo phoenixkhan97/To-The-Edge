e4a512022d88b4893e0d6a609d16d6c819898ad8
Sahidan Khan <sahidankhan7991@gmail.com>
2022-07-06 10:27:45 -0500

    gravity completed

--- script.js
+++ script.js
@@ -4,28 +4,52 @@ const canvas = document.getElementById("canvas")
 const c = canvas.getContext(`2d`)
 canvas.height = innerHeight
 canvas.width = innerWidth
+const gravity = 0.5
 
-
-class Player{
+class Wizard{
     constructor(){
-        this.position = {
+        this.axis = {
             x:300,
             y:160
         }
         this.width = 50
         this.height = 50
+
+        this.velocity = {
+            x:0,
+            y:0
+        }
     }
 
     draw(){
         c.fillStyle = 'green'
-        c.fillRect(this.position.x,this.position.y,this.width,this.height)
+        c.fillRect(this.axis.x,this.axis.y,this.width,this.height)
+    }
+
+    playerChange(){
+        this.axis.y += this.velocity.y
+        this.draw()
+        if(this.axis.y + this.height + this.velocity.y <= canvas.height)
+
+        this.velocity.y += gravity
+        else
+        this.velocity.y = 0
     }
 }
 
-const player = new Player();
+const player = new Wizard();
 
-player.draw()
+player.playerChange()
 
+
+
+fallAnimation = () => {
+    requestAnimationFrame(fallAnimation)
+    c.clearRect(0,0,canvas.width,canvas.height)
+    player.playerChange()
+}
+
+fallAnimation()
 //Player
 
 //this.position = {
