e23cc46a294bf0e7a170a669a71237ee11ac1bad
sahidankhan <sahidankhan7991@gmail.com>
2022-07-07 17:27:22 -0500

    pictures completed

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-
 const canvas = document.getElementById("canvas")
-
 const c = canvas.getContext(`2d`)
 canvas.height = innerHeight
 canvas.width = innerWidth
-const gravity = 0.4
-
-// preload = () =>{
-// this.load.image(`ground`, link.png)
-// }
+const gravity = 0.4 
 
 
+
+let tileImage = new Image()
+tileImage.src="./pics/tileexample.png"
+
+let tallImage = new Image()
+tallImage.src="./pics/tilebackup.png"
+
+let wizardImage = new Image()
+wizardImage.src="./pics/steven2.png"
 
 class Wizard{
     constructor(){
@@ -19,17 +22,17 @@ class Wizard{
             y:200
         }
         this.width =40
-        this.height = 60
+        this.height = 40
 
         this.velocity = {
             x:0,
             y:0
         }
+        this.image = wizardImage
     }
 
     draw(){
-        c.fillStyle = 'green'
-        c.fillRect(this.axis.x,this.axis.y,this.width,this.height)
+        c.drawImage(wizardImage,this.axis.x,this.axis.y,this.width,this.height)
     }
 
     playerChanges(){
@@ -39,11 +42,30 @@ class Wizard{
         if(this.axis.y + this.height + this.velocity.y <= canvas.height)
         this.velocity.y += gravity
 
-        else
-
-        this.velocity.y = 0 
     }
 }
+
+
+class Platform{
+    constructor({x,y}){
+        this.axis = {
+            x,
+            y
+        }
+        this.width = 500,
+        this.height = 30
+        this.image = tileImage
+    }
+    draw(){
+        
+        c.drawImage(tileImage,this.axis.x,this.axis.y,this.width,this.height)
+    }
+}
+
+const platforms =[
+    new Platform({x:0,y:525,tileImage}),
+    new Platform({x:400,y:525, tileImage}),
+new Platform({x:800,y:325,tallImage})]
 
 
 const player = new Wizard();
@@ -54,29 +76,7 @@ const movement = {
     left:{
         pressed:false
     }
-}
-
-
-
-
-class Platform{
-    constructor({x,y}){
-        this.axis = {
-            x,
-            y
-        }
-        this.width = 200,
-        this.height = 50
-    }
-    draw(){
-        c.fillStyle = `blue`
-        c.fillRect(this.axis.x, this.axis.y, this.width, this.height)
-    }
-}
-
-const platforms =[new Platform({x:20,y:600}),new Platform({x:400,y:550}),new Platform({x:600,y:300}),new Platform({x:900,y:600}),new Platform({x:1200,y:300})]
-
-
+} 
 
 
 fallAnimation = () => {
@@ -93,7 +93,7 @@ let scroll = 0
 
     if(movement.right.pressed && player.axis.x <500){
         player.velocity.x = 4
-    }else if(movement.left.pressed && player.axis.x > 50){
+    }else if((movement.left.pressed && player.axis.x > 10)||(movement.left.pressed && player.axis.x < 0)){
         player.velocity.x = -4
     }
     else
@@ -103,16 +103,18 @@ let scroll = 0
         scroll += 4
         platforms.forEach(platform =>{
         platform.axis.x -= 4
-        })}else if (movement.left.pressed){
+        })}else if (movement.left.pressed && scroll > 0){
         scroll = -4
         platforms.forEach(platform =>{
         platform.axis.x += 4
     }
     )}
+    
 
     if(scroll > 2000){
         console.log(`you win`)
     }
+  
     
 
 
@@ -121,11 +123,12 @@ let scroll = 0
         if(player.axis.y + player.height <= platform.axis.y && player.axis.y + player.height + player.velocity.y >= platform.axis.y && player.axis.x + player.width >= platform.axis.x && player.axis.x <= platform.axis.x + platform.width){
     player.velocity.y = 0
 }
+
+
     })}
 
     fallAnimation()
-
-
+    
 
 
 addEventListener(`keydown`,({ keyCode }) =>{
@@ -137,15 +140,16 @@ addEventListener(`keydown`,({ keyCode }) =>{
         break;
         case 87:
             console.log(`up`)
-            player.velocity.y -= 10
+            player.velocity.y -= 6
         break;
         case 83:
             console.log(`down`)
         break;
         case 68:
             console.log(`right`)
-            movement.right.pressed = true
             player.velocity.x = 0
+            movement.right.pressed = true
+            
         break;
     }
 })
